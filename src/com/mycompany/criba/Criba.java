b0f6592f602bb554b73fba7b408ef7c6e5320270
AntonioAOrtiz <73552282+AntonioAOrtiz@users.noreply.github.com>
2021-03-09 16:47:56 +0100

    Creación del método criba

--- src/com/mycompany/criba/Criba.java
+++ src/com/mycompany/criba/Criba.java
@@ -30,21 +30,19 @@ public class Criba {
 	* @return Vector de números primos
 	*/
 	public int[] generarPrimos (int max) {
-		int i,j;
 		if (max >= 2) {
 			// Declaraciones
-			int dim = max + 1; // Tamaño del array
-			boolean[] esPrimo = new boolean[dim];
+			max++; // Tamaño del array
+			boolean[] esPrimo = new boolean[max];
 			
 			// Inicializar el array
-			for (i=0; i<dim; i++)
-				esPrimo[i] = true;
+			for (int i=2; i<max; i++) {
+				esPrimo[i] = true;}
 			
 			// Eliminar el 0 y el 1, que no son primos
-			esPrimo[0] = esPrimo[1] = false;
 			
 			// Criba
-			for (i=2; i<Math.sqrt(dim)+1; i++) {
+			for (int i=2; i<Math.sqrt(max)+1; i++) {
 				if (esPrimo[i]) {
 					// Eliminar los múltiplos de i
 					for (j=2*i; j<dim; j+=i)
@@ -54,14 +52,14 @@ public class Criba {
 			
 			// ¿Cuántos primos hay?
 			int cuenta = 0;
-			for (i=0; i<dim; i++) {
+			for (int i=0; i<max; i++) {
 				if (esPrimo[i])
 				cuenta++;
 			}
 			
 			// Rellenar el vector de números primos
 			int[] primos = new int[cuenta];
-			for (i=0, j=0; i<dim; i++) {
+			for (int i=0,int j=0; i<max; i++) {
 				if (esPrimo[i])
 				primos[j++] = i;
 			}
